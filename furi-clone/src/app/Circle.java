939cad557ea4af2eaafaf48b5d74ca7402296202
N C <nchan540@gmail.com>
2020-02-13 14:53:58 -0500

    Commented code, added new shape methods

--- furi-clone/src/app/Circle.java
+++ furi-clone/src/app/Circle.java
@@ -2,19 +2,79 @@ package app;
 
 public class Circle implements Shape {
 
+    // Location of circle
     public Point p;
+    //Circle radius
     public int radius;
 
+    /**
+     * Constructor for a circle of radius r located at point p
+     * @param p location of circle
+     * @param r radius of circle
+     */
     Circle(Point p, int r) {
         this.p = new Point(p.x, p.y);
         this.radius = r;
     }
 
+    /**
+     * Constructor for a circle of radius r located at P(x, y)
+     * @param x x coordinate of circle
+     * @param y y coordinate of circle
+     * @param r radius of circle
+     */
     Circle(float x, float y, int r) {
         this.p = new Point(x, y);
         this.radius = r;
     }
 
+    /**
+     * Checks if overlap occurs with another circle
+     * @param target circle being checked against
+     * @return true or false for overlap
+     */
+    public boolean checkOverlap(Circle target) {
+        double targetRadius = Math.pow(target.getArea() / Math.PI, 1.0/2);
+        if (this.getDistance(target.p) <= targetRadius) return true;
+        return false;
+    }
+
+    /**
+     * Checks if overlap occurs with a line segment
+     * @param target line segment being checked against
+     * @return true or false for overlap
+     */
+    public boolean checkOverlap(LineSegment target) {
+        LineSegment tangent = new LineSegment(new Line(-1 / target.equation.m, this.p), this.p, this.radius);
+
+        return (tangent.checkOverlap(target));
+    }
+
+    /**
+     * Finds the distance between a point P and the point on the circle closest to point P
+     * @param p point being checked
+     * @return distance between the point and the circle's closest point
+     */
+    public float getDistance(Point p) {
+        return (float)(Math.sqrt(Math.pow(this.p.x - p.x, 2) + Math.pow(this.p.y-p.y, 2))) - this.radius;
+    }
+
+    /**
+     * Gets the location of the circle
+     * @return location of the circle
+     */
+    public Point getLocation() { return this.p; }
+
+    /**
+     * Gets the area of the circle
+     * @return area of the circle
+     */
+    public double getArea() { return Math.pow(this.radius, 2) * Math.PI;}
+
+    /**
+     * Gets the x coordinates at a y location
+     * @return array of the 2 x coordinates
+     */
     public float[] getXatY(int y) {
         if(Math.abs(y-p.y) > radius) return new float[]{-1000};
         float[] coords = new float[2];
@@ -23,8 +83,11 @@ public class Circle implements Shape {
         return coords;
     }
 
+    /**
+     * Gets the y coordinates at an x location
+     * @return array of the 2 y coordinates
+     */
     public float[] getYatX(int x) {
-
         if(Math.abs(x-p.x) > radius) return new float[]{-1000};
 
         float[] coords = new float[2];
