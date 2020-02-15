0d7d2f127fa6cb72c52eb31d82482c86e30e14a0
nchan540 <nchan540@gmail.com>
2020-02-15 17:49:44 -0500

    Added constructor for 2 points

--- furi-clone/src/graph/Line.java
+++ furi-clone/src/graph/Line.java
@@ -32,6 +32,17 @@ public class Line {
         this.b = p.y - this.m * p.x;
     }
 
+    public Line(Point_ p1, Point_ p2) {
+        if (p1.x == p2.x) p2.x += 0.00001f;
+        this.m = (p2.y - p1.y) / (p2.x - p1.x);
+        this.b = p1.y - m*p1.x;
+
+        if (0 <= this.m && this.m < 0.00001f) this.m = 0.00001f;
+        else if (-0.00001f < this.m && this.m < 0f) this.m = 0.00001f;
+        else if (this.m > 10000) this.m = 10000;
+        else if (this.m < -10000) this.m = -10000;
+    }
+
     /**
      * Returns the y value at an x coordinate
      * @param x x coordinate
