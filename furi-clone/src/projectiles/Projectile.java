35c313131413144389d0a07fdd5f6744d006c313
nchan540 <nchan540@gmail.com>
2020-02-15 17:50:58 -0500

    Added alternative setSpeed method

--- furi-clone/src/projectiles/Projectile.java
+++ furi-clone/src/projectiles/Projectile.java
@@ -30,6 +30,18 @@ public abstract class Projectile {
         this.ySpeed = deltaY * ratio;
     }
 
+    public void setSpeed(float speed) {
+        float deltaY = direction.getY(1) - direction.getY(0);
+        float deltaX = 1;
+
+        // 1 (deltaX) squared is 1, and deltaX will always be 1, so the value can be put it to improve speed
+        // the ratio of speed : hypotenuse is the same as the ratio of xSpeed : deltaX and ySpeed : deltaY
+        float ratio = speed / (float)(Math.abs(Math.sqrt(1 + Math.pow(deltaY, 2))));
+        // speed / xSpeed = deltaX / ratio
+        this.xSpeed = deltaX * ratio;
+        this.ySpeed = deltaY * ratio;
+    }
+
     public void move() {
         this.hitbox.getLocation().x += xSpeed;
         this.hitbox.getLocation().y += ySpeed;
