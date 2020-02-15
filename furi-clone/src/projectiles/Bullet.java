9cf92d8d076d62a2c2a3d02bc127fa5e5639e4cf
nchan540 <nchan540@gmail.com>
2020-02-14 21:27:32 -0500

    Added hitDetect

--- furi-clone/src/projectiles/Bullet.java
+++ furi-clone/src/projectiles/Bullet.java
@@ -7,4 +7,8 @@ public class Bullet extends Projectile {
     public Bullet(float x, float y, Line direction) {
         super(new Circle(x, y, 10), 10, direction);
     }
+
+    public boolean hitDetect(Shape target) {
+        return false;
+    }
 }
